e3e7f8ccf135b6941d6af06c95618d5ed0104da3
longqwerty <43800613+qwerty2291999@users.noreply.github.com>
2021-03-23 13:26:35 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,9 @@
 class products{
-	constructor(name,price,image,discountPrice,discount,hot,quantity){
+	constructor(name,price,image,discountPrice,quantity){
 		this.name = name;
 		this.price = price;
 		this.image = image;
 		this.discountPrice = discountPrice;
-		this.discount = discount;
-		this.hot = hot;
 		this.quantity = quantity;
 	}
 }
@@ -17,23 +15,26 @@ class Basket {
   addItem(item){
     this.items.push(item)
   }
+  removeItem(index){
+    this.items.splice(index,1);
+  }
 }
 
-let basket = new Basket();
+var basket = new Basket();
 
 
-var product1 = new products("Product 1","59","img/product-23.jpg",50,true,true,1);
-var product2 = new products("Product 2","59","img/product-23.jpg",50,false,true,1);
-var product3 = new products("Product 3","59","img/product-23.jpg",50,true,false,1);
-var product4 = new products("Product 4","59","img/product-23.jpg",50,false,true,1);
-var product5 = new products("Product 5","$59","img/product-23.jpg",50,true,true,1);
-var product6 = new products("Product 6","$59","img/product-23.jpg",50,false,false,1);
-var product7 = new products("Product 7","$59","img/product-23.jpg",50,true,true,1);
-var product8 = new products("Product 8","$59","img/product-23.jpg",50,true,true,1);
-var product9 = new products("Product 9","$59","img/product-23.jpg",50,true,true,1);
-var product10 = new products("Product 10","$59","img/product-23.jpg",50,true,true,1);
-var product11 = new products("Product 11","$59","img/product-23.jpg",50,true,true,1);
-var product12 = new products("Product 12","$59","img/product-23.jpg",50,true,true,1);
+var product1 = new products("Product 1","59","img/product-21.jpg",50,1);
+var product2 = new products("Product 2","59","img/product-22.jpg",50,1);
+var product3 = new products("Product 3","59","img/product-23.jpg",50,1);
+var product4 = new products("Product 4","59","img/product-24.jpg",50,1);
+var product5 = new products("Product 5","$59","img/product-21.jpg",50,1);
+var product6 = new products("Product 6","$59","img/product-22.jpg",50,1);
+var product7 = new products("Product 7","$59","img/product-23.jpg",50,1);
+var product8 = new products("Product 8","$59","img/product-24.jpg",50,1);
+var product9 = new products("Product 9","$59","img/product-21.jpg",50,1);
+var product10 = new products("Product 10","$59","img/product-22.jpg",50,1);
+var product11 = new products("Product 11","$59","img/product-23.jpg",50,1);
+var product12 = new products("Product 12","$59","img/product-24.jpg",50,1);
 var items = [product1,product2,product3,product4,product5,product6,product7,product8,product9,product10,product11,product12];
 var totalDistance = [];
 var leftArrow = document.getElementById("left-arrow");
@@ -41,21 +42,25 @@ var rightArrow = document.getElementById("right-arrow");
 var slider = document.getElementById("product-items");
 
 function addItem(index) {
-  	basket.addItem(items[index])
+  	basket.addItem(items[index]);
   	showBasket()
 }
-
+function removeItem(index) {
+    basket.removeItem(index)
+    showBasket()
+}
 function showBasket() {
   let cart = document.getElementById("cart-list");
-  var c = [... new Set(basket.items)];
-  var s = c.map((item,index)=>"<div class=cart-item><div class=cart-info><span class=name><a href=# title=name>"+item.name+"</a></span><span class=cart-price>"+item.discountPrice+"</span><span class=cart-total>Total : "+item.discountPrice+"</span><div class=increament><span class=minus>-</span><input type=text name=a class=input value="+item.quantity+"><span class=plus>+</span></div></div><div class=cart-image><a href=# title=img><img src="+item.image+" alt=img></a></div></div></div>").join("");
+  var c = basket.items;
+  var s = basket.items.map((item,index)=>"<div class=cart-item><div class=cart-info><span class=name><a href=# title=name>"+item.name+"</a></span><span class=cart-price>"+item.discountPrice+"</span><span class=cart-total>Total : "+item.discountPrice+"</span><div class=increament><span class=minus>-</span><input type=text name=a class=input value="+item.quantity+"><span class=plus>+</span></div></div><div class=cart-image><span class=remove onclick=removeItem("+index+")>X</span><a href=# title=img><img src="+item.image+" alt=img></a></div></div></div>").join("");
   cart.innerHTML = s;
   var count = document.getElementById("count");
   var countCart = document.getElementById("count-cart");
   count.innerHTML = c.length+" Items";
   countCart.innerHTML = c.length;
-
+  var getTotal = document.querySelectorAll(".cart-total");
   var prices = document.querySelectorAll(".cart-price");
+  var innerTotal = document.getElementById("total");
   for(price of prices){
     e = parseInt(price.innerText);
   }
@@ -68,9 +73,7 @@ function showBasket() {
         var d = b.querySelector(".cart-total")
         c.value++;
         total = c.value*e;
-        d.innerHTML = "Total : "+total;
-        var innerTotal = document.getElementById("total");
-        var getTotal = document.querySelectorAll(".cart-total");
+        d.innerHTML = "Total : "+total;     
         var arr = [];
        for(per of getTotal){
           let abc = per.innerText;
@@ -81,7 +84,6 @@ function showBasket() {
           var result = arr.reduce((a, b) => a + b, 0)
           innerTotal.innerHTML = "Total Price : "+result+"$";
       }
-      
     }
 var btnMinus = document.querySelectorAll(".minus");
     for(minus of btnMinus) {
@@ -95,8 +97,6 @@ var btnMinus = document.querySelectorAll(".minus");
         }
         total = c.value*e;
         d.innerHTML = "Total : "+total;
-        var innerTotal = document.getElementById("total");
-        var getTotal = document.querySelectorAll(".cart-total");
         var arr = [];
        for(per of getTotal){
           let abc = per.innerText;
@@ -108,14 +108,21 @@ var btnMinus = document.querySelectorAll(".minus");
           innerTotal.innerHTML = "Total Price : "+result+"$";
       }
     }
+    var arr = [];
+       for(per of getTotal){
+          let abc = per.innerText;
+          var cutString = abc.substring(8)
+          var stringToNum = parseInt(cutString);
+          arr.push(stringToNum);
+          }
+          var result = arr.reduce((a, b) => a + b, 0)
+          innerTotal.innerHTML = "Total Price : "+result+"$";
 }
-
-
 var market = document.getElementById("product-items");
 let add = items.map((item, index)=>"<div class=item><div class=item-img><a href=#><img src="+item.image+"></a><div class=add-item onClick=addItem("+index+")><p>Add</p></div></div><div class=item-info><a href=#>"+item.name+"</a><div class=price-box><span class=old-price>"+item.price+"$"+"</span><span class=price>"+item.discountPrice+"$"+"</span></div></div></div>").join("");
 market.innerHTML = add;
 
-rightArrow.onclick = function(e) {
+rightArrow.onclick = function(e){
 	slider.scrollLeft += 295;
 	totalDistance.push(295);
 	let keoPhai = 295;
@@ -129,10 +136,6 @@ leftArrow.onclick = function(e) {
 	slider.scrollLeft -= 295;
 	totalDistance.pop();
 }
-
-
-
-
 var a = 0;
 var slide = document.getElementsByClassName("slider-image");
 setInterval(function(){
@@ -164,14 +167,13 @@ btn.onclick = function(event){
 alert.onclick = function(){
 	alert.style.display = 'none';
 }
-
-// var cCart = document.getElementById("basket");
-// var cart = document.getElementById("cart")
-// cCart.onclick =function(){
-//     if(cart.style.display == 'block'){
-//         cart.style.display = 'none';
-//     }
-//     else{
-//       cart.style.display = 'block';
-//     }
-// }
+$( ".add-item" ).click(function() {
+  $( "#add-notification" ).fadeIn(1000, function(){
+    setTimeout(
+  function() 
+  {
+    $("#add-notification").fadeOut(3000,function(){
+    })
+  }, 1000);
+  });
+});
